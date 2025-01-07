c2ac96c8e66b1c57ca81fd4d40dd0cf96dd9e645
rithikasuresh <srithika1998@gmail.com>
2025-01-07 20:04:49 +0530

    removed

--- tax_calculator/build/Qt_6_8_1_for_macOS-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_tax_calculator_FILE /Users/rithikasuresh/qt_projects/tax_calculator/build/Qt_6_8_1_for_macOS-Debug/tax_calculator.app/Contents/MacOS/tax_calculator)
-set(__QT_DEPLOY_TARGET_tax_calculator_TYPE EXECUTABLE)
